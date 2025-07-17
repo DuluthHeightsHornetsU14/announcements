9ca9e6ab84093b7b1c2175d5eb1d3f87b730d2af
DuluthHeightsHornetsU14 <david.solomon.pearlman@gmail.com>
2025-07-16 20:22:45 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'soccer-v1';
+const urlsToCache = ['/', '/index.html', '/data.json', '/manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (!response || response.status !== 200) {
+          return caches.match(event.request);
+        }
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, responseToCache));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
